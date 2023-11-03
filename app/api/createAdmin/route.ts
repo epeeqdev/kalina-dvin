9b35f570e2ef7ddd8f47543e37ac2b28e0301088
rafnkg <nikoghosyanrafayell@gmail.com>
2023-11-03 22:41:37 +0400

    Add createAdmin

---
+++ app/api/createAdmin/route.ts
@@ -0,0 +1,33 @@
+import {DB} from "@/backend/db";
+import CryptoJS from "crypto-js"
+import {NextResponse} from "next/server";
+
+export async function GET(request: Request,) {
+	try{
+		const hashedPassword = await CryptoJS.AES.encrypt(
+			'1234',
+			process.env.PASS_SEC
+		);
+		console.log('hashedPassword',hashedPassword)
+		const user = await new DB.User(
+			{
+				username: 'admin',
+				password: hashedPassword
+			}
+		).save()
+
+		if(!user){
+			return new NextResponse("Wrong User Name", {status: 401})
+		}
+
+
+
+		return NextResponse.json({});
+
+	}catch(err){
+		console.log(err,'err')
+		return new NextResponse(JSON.stringify({message: "Something went wrong on our side."}), {status: 500})
+	}
+}
+
+
