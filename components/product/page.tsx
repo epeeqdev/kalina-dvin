37350e67feec8ff4a77ef1c64de51c0cec321c2d
rafnkg <nikoghosyanrafayell@gmail.com>
2023-11-04 05:13:10 +0400

    refactor

--- components/product/page.tsx
+++ components/product/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {Product} from "@/app/admin/main/products/types";
 
 interface Props {
@@ -9,7 +10,7 @@ export const ProductTemplate = ({item}: Props) => {
     return (
             <div className='w-full bg-white hover:bg-[#eeeeee] transition flex gap-[2%] py-2 px-2'>
                 <div className='relative'>
-                    <img className='w-[150px] h-[150px] min-w-[150px] object-contain bg-[#dadada] rounded left-0 top-0' src={require(item.images[0]?.src)} alt="photo"/>
+                    <img className='w-[150px] h-[150px] min-w-[150px] object-contain bg-[#dadada] rounded left-0 top-0' src={item.images[0]?.src} alt="photo"/>
                 </div>
                 <div className='pr-4'>
                     <h3 className='text-[21px] mb-2'>{item.title}</h3>
