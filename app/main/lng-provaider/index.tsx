86aee27b6d15ee3d8451dddcdc4ddc1765776f1a
Artak <artak199393@gmail.com>
2023-11-07 19:48:29 +0400

    fixed bild error

--- app/main/lng-provaider/index.tsx
+++ app/main/lng-provaider/index.tsx
@@ -1,12 +1,19 @@
 'use client'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {MainContextProvider} from "@/app/main/provaider";
 
 const DEFAULT_STATE = 'arm'
 
 export const LngProvider = React.memo(({ children }: any) => {
-    const lng = localStorage.getItem('lng')
-    const language = lng  ? lng : DEFAULT_STATE
+    const [defaultLng, setDefaultLng] = useState('arm')
+    useEffect(() => {
+        const selectLng = localStorage.getItem('lng')
+        if(selectLng) {
+            setDefaultLng(selectLng)
+        }
+    },[])
+
+    const language = defaultLng  ? defaultLng : DEFAULT_STATE
     return (
         <MainContextProvider defaultValue={language} >
             {children}
