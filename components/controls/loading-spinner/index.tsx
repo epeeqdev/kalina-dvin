b9daf5d75ba9cf922ecfe35f873117094e8cf01b
BlueLineMonster <rafayelnikoghosyan@gmail.com>
2023-11-17 19:24:55 +0400

    modify side bar and add burger menu animation

--- components/controls/loading-spinner/index.tsx
+++ components/controls/loading-spinner/index.tsx
@@ -4,7 +4,7 @@ import ReactLoading from "react-loading";
 
 export default function LoadingSpinner() {
     return (
-        <div className="w-full fixed left-0 top-0 h-full z-50 bg-black bg-opacity-50 text-white flex justify-center items-center text-lg">
+        <div className="w-full fixed left-0 top-0 h-full z-[70] bg-black bg-opacity-50 text-white flex justify-center items-center text-lg">
             <ReactLoading type="spin" color="red" />
         </div>
     );
