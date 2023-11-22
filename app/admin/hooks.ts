45b5294ffa79cb1e4ee12cfb65dc98f6f42223b3
epeeq <epeeqdv@gmail.com>
2023-11-22 21:16:25 +0400

    Login

--- app/admin/hooks.ts
+++ app/admin/hooks.ts
@@ -3,11 +3,13 @@ import {useUserContext} from "@/app/admin/userContext";
 import { useRouter } from 'next/navigation'
 import {useMutation} from "@/utils/hooks/useMutation";
 import {adminLogin} from "@/app/admin/helpers/api/admin-login";
+import {useEffect, useState} from "react";
 
 export const useAuth = () => {
+    const [isInitialized, setInitialized] = useState(false)
     const userContext = useUserContext();
     const router = useRouter();
-    const {mutate: loginMutate} = useMutation(adminLogin)
+    const {mutate: loginMutate, isLoading} = useMutation(adminLogin)
     const login = async (credentials: Credentials) => {
         try {
             const user = await loginMutate(credentials);
@@ -15,13 +17,21 @@ export const useAuth = () => {
             window.localStorage.setItem('accessToken', user.accessToken)
         }catch (e){
             console.log(e)
+        }finally {
+            setInitialized(true);
         }
     }
+
+    useEffect(() => {
+        if(!window.localStorage.getItem('accessToken')){
+            setInitialized(true)
+        }
+    }, []);
     const logout = async () => {
         window.localStorage.removeItem('accessToken');
         userContext.setData(null);
         router.push('/admin/login');
     }
 
-    return {login, logout}
+    return {login, logout, isLoading, isInitialized}
 }
--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -9,6 +9,7 @@ import {useUserContext} from "@/app/admin/userContext";
 import { useRouter } from 'next/navigation'
 import {useEffect} from 'react'
 import {Button} from "../main/components/controls/button";
+import LoadingSpinner from "@/components/controls/loading-spinner";
 
 const validationSchema = yup.object().shape({
 	username: yup.string().required("Username is required"),
@@ -20,23 +21,24 @@ export default function Login(){
 	});
 	const userContext = useUserContext();
 	const router = useRouter();
-	const {login} = useAuth();
+	const {login, isLoading, isInitialized} = useAuth();
 	useEffect(() => {
 		if(userContext.data){
-			router.replace("/admin/main")
+			router.replace("/admin/main");
 		}
-	},[userContext.data])
+	},[userContext.data, isLoading])
 
-
+	if(!isInitialized) return null;
 	return <div className='flex justify-center items-center min-h-screen px-5'>
+		{isLoading && <LoadingSpinner/>}
 		<form onSubmit={handleSubmit(login)} className='w-full max-w-[500px] flex flex-col justify-center items-stretch gap-y-[10px]'>
 		<div>
-			<Input label='Username' placeholder='Enter username' {...register('username')} className='w-full'/>
+			<Input label='Логин' placeholder='Введите логин' {...register('username')} className='w-full'/>
 		</div>
 		<div>
-			<Input label='Password' type='password' placeholder='Enter password' {...register('password')} className='w-full'/>
+			<Input label='Пароль' type='password' placeholder='Введите пароль' {...register('password')} className='w-full'/>
 		</div>
-		<Button type='submit' className='mt-2' variant="primary">Login</Button>
+		<Button type='submit' className='mt-2' variant="primary">Войти</Button>
 	</form>
 	</div>
 }
