cdec12210f3f5a81c2cb44381717987bc122e566
epeeq <epeeqdv@gmail.com>
2023-11-22 22:44:30 +0400

    Remove overflow hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{overflow: "hidden"}}>{children}</body>
+      <body className={inter.className} style={{overflowX: "hidden"}}>{children}</body>
     </html>
   )
 }
