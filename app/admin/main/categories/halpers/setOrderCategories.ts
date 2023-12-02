4823514f2c1f4a5bdf0c86f0d792dc5e674407f4
Artak <artak199393@gmail.com>
2023-12-02 04:21:25 +0400

    fix order brands

--- app/admin/main/categories/halpers/setOrderCategories.ts
+++
@@ -1,4 +0,0 @@
-import axios from "@/axios";
-import {API_ROUTES} from "@/app/api/constants";
-
-export const setOrderCategories = (order : string[]) => axios.put(API_ROUTES.reorder.categories, {order});
