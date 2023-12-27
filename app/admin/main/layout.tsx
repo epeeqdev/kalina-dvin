c5b5d7e912e770b1727b88fa5b5820f52c975d9b
BlueLineMonster <rafayelnikoghosyan@gmail.com>
2023-12-27 19:39:32 +0400

    adding language switcher to admin side

--- app/admin/main/layout.tsx
+++ app/admin/main/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 import {PropsWithChildren, useEffect, useRef, useState} from "react";
-import Link from "next/link";
 import {useAuth} from "@/app/admin/hooks";
 import {RouteGuard} from "@/app/admin/RouteGuard";
 import Hamburger from 'hamburger-react'
@@ -10,6 +9,7 @@ import Accordion from "@/app/admin/main/components/accordion";
 import {usePathname} from "next/navigation";
 import SideBarLink from "./components/sideBarLink";
 import {Button} from "@/app/admin/main/components/controls/button";
+import LngSwitcher from "./components/lngSwitcher";
 
 export default function Layout({children}:PropsWithChildren){
 	const isMobile = useMatchMedia('(max-width: 639.1px)');
@@ -90,6 +90,9 @@ export default function Layout({children}:PropsWithChildren){
 			<div className='flex min-h-screen overflow-hidden'>
 				<div className={`fixed z-[40] sm:relative sm:mt-0  sm inset-y-0 left-0 bg-gray-800 text-white ${isOpen ? "w-[200px] sm:w-[350px]" : "w-0 sm:w-0"} transition-all duration-300`}>
 					<div className={`mt-20 sm:mt-5 overflow-hidden ${isOpen ? "opacity-1 transition-all duration-300" : "opacity-0 transition-all duration-300"} `}>
+						<div className={`flex h-full mb-5 content-between justify-end items-end ${isOpen ? "opacity-1 transition-all duration-300" : "opacity-0 transition-all duration-300"}` }>
+							<LngSwitcher />
+						</div>
 						<div className="block px-4 pb-10 text-[18px] transition whitespace-nowrap">Меню</div>
 						<SideBarLink title="Главная" chosenNamePath="admin/main" slicedPathName={slicedPathName} className={styles} handleClick={handleClick} href='/admin/main' />
 						<SideBarLink title="Категории" chosenNamePath="main/categories" slicedPathName={slicedPathName} className={styles} handleClick={handleClick} href='/admin/main/categories'  />
@@ -111,7 +114,6 @@ export default function Layout({children}:PropsWithChildren){
 									!isLaptop ? setIsOpen(false) : null
 							}}/>
 						</div>
-
 					</div>
 				</div>
 				<div className='w-full' ref={burgerRef}>
